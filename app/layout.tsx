c621b0966159069d17a9e8f3ece0ffadd8ad8484
axixiansheng <110860393+axixiansheng@users.noreply.github.com>
2023-04-25 22:06:08 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "仅供个人使用",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "仅供个人使用",
     statusBarStyle: "black-translucent",
   },
   themeColor: "#fafafa",
